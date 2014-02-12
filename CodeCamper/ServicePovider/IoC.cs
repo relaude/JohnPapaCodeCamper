c9c5b384be671b95eba24334acaf5cb8197af8c7
relaude <laude.ramil@gmail.com>
2014-02-12 21:30:15 +0800

    Add Service

---
+++ CodeCamper/ServicePovider/IoC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StructureMap;
+
+namespace ServicePovider
+{
+    public static class IoC
+    {
+        public static IContainer Initialize()
+        {
+            ObjectFactory.Initialize(x =>
+            {
+                x.Scan(scan =>
+                {
+                    scan.TheCallingAssembly();
+                    scan.WithDefaultConventions();
+                });
+                x.For<Repository.IUnitOfWork>().Use<UnitOfWork<DataAccess.UsersAccountDbContext>>();
+            });
+            return ObjectFactory.Container;
+        }
+    }
+}
---
+++ CodeCamper/ServicePovider/UserAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess;
+using Repository.UserAccount;
+
+namespace ServicePovider
+{
+    public class UserAccount
+    {
+        private readonly UnitOfWork<UsersAccountDbContext> _uow;
+        private readonly UserProfileRepository _user;
+        private readonly webpages_RolesRepository _roles;
+        private readonly webpages_UsersInRolesRepository _inRoles;
+
+        public UserAccount()
+        {
+            _uow = new UnitOfWork<UsersAccountDbContext>();
+            _user = new UserProfileRepository(_uow);
+            _roles = new webpages_RolesRepository(_uow);
+            _inRoles = new webpages_UsersInRolesRepository(_uow);
+        }
+
+        public UserProfileRepository UserRepo
+        {
+            get { return _user; }
+        }
+
+        public webpages_RolesRepository RoleRepo
+        {
+            get { return _roles; }
+        }
+
+        public webpages_UsersInRolesRepository InRoleRepo
+        {
+            get { return _inRoles; }
+        }
+
+        public void Save()
+        {
+            _uow.Save();
+        }
+    }
+}
